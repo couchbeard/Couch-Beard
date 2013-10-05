f1e68003afcd135b8bcb12ba2466ba693022d1fc
madslundt <madssx@hotmail.com>
2013-10-06 00:27:23 +0200

    Footer

--- wp-content/themes/couchbeard/footer.php
+++ wp-content/themes/couchbeard/footer.php
@@ -6,26 +6,26 @@
 		   			 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed"><img alt="Creative Commons licens" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a>
 		    	</div>
 		    	<div class="span1 pull-right">
-					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="pull-right">
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 					<input type="hidden" name="cmd" value="_donations">
-					<input type="hidden" name="business" value="madssx@hotmail.com">
-					<input type="hidden" name="lc" value="US">
-					<input type="hidden" name="item_name" value="Couch beard">
+					<input type="hidden" name="business" value="madslundt@live.dk">
+					<input type="hidden" name="lc" value="DK">
+					<input type="hidden" name="item_name" value="CouchBeard">
 					<input type="hidden" name="no_note" value="0">
 					<input type="hidden" name="currency_code" value="EUR">
 					<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<input type="image" src="https://www.paypalobjects.com/da_DK/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal – den sikre og nemme måde at betale på nettet.">
 					<img alt="" border="0" src="https://www.paypalobjects.com/da_DK/i/scr/pixel.gif" width="1" height="1">
 					</form>
 				</div>
 			</div>
 		</footer>
-		<div id="statusConnection" data-toggle="tooltip">
-			<span class="label label-warning">
-				<p class="pull-right">Connection status</p>
-				<p class="pull-left"><i class="icon-minus-sign icon-white"></i></p>
-			</span>
-		</div>
+			<div id="statusConnection" data-toggle="tooltip">
+				<span class="label label-warning">
+					<p class="pull-right">Connection status</p>
+					<p class="pull-left"><i class="icon-minus-sign icon-white"></i></p>
+				</span>
+			</div>
 		</div> <!-- /container -->
 		<?php wp_footer(); ?>
 	</body>
